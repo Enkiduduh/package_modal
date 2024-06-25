44d3292e2be73274e3f13b837d213ac5e0f7fcc7
Ilyes Soumar Djouma <ilyes.soumar@free.fr>
2024-06-25 17:49:37 +0200

    updated modal.css

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,1 @@
 declare module '@enkiduh/package_modal';
-
-// declare module "Modal" {
-//   function Modal(): void;
-//   export = Modal;
-// }
--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import './Modal.css';
 
 function Modal({
   isFormSubmitted,
